f55cb474ca2e3f98af364b12f94de2ed7b5efe9f
Aarón <aaron@MacBook-Pro.local>
2025-12-27 23:17:50 +0000

    exercise 3.7 & 3.8

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+var morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
