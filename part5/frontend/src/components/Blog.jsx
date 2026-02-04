452e34898926adc2b278a101ccb4e36140017ec0
Aarón <aaron@MacBook-Pro.local>
2026-02-04 19:00:25 +0000

    part 5b 5.8

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,25 +1,31 @@
 import Togglable from "./Togglable";
+import blogService from "../services/blogs";
 
-const Blog = ({ blog }) => (
-  <div className="blogItem">
-    {blog.title}
-    <Togglable buttonLabel="view">
-      <p>
-        <a href={blog.url}>{blog.url}</a>
-      </p>
-      <p>
-        Likes: {blog.likes}{" "}
-        <button
-          onClick={() => {
-            console.log("like!");
-          }}
-        >
-          like
-        </button>
-      </p>
-      <p>{blog.author}</p>
-    </Togglable>
-  </div>
-);
+const Blog = ({ blog }) => {
+  const likeBlog = async (blog) => {
+    await blogService.update(blog.id, {
+      user: blog.user,
+      likes: blog.likes + 1,
+      author: blog.author,
+      title: blog.title,
+      url: blog.url,
+    });
+  };
+  return (
+    <div className="blogItem">
+      {blog.title}
+      <Togglable buttonLabel="view">
+        <p>
+          <a href={blog.url}>{blog.url}</a>
+        </p>
+        <p>
+          Likes: {blog.likes}{" "}
+          <button onClick={() => likeBlog(blog)}>like</button>
+        </p>
+        <p>{blog.author}</p>
+      </Togglable>
+    </div>
+  );
+};
 
 export default Blog;
