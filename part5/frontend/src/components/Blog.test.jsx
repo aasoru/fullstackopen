e3d9ffc47aecb5539fc61cfcc90bfb41f9cd2084
Aarón <aaron@MacBook-Pro.local>
2026-02-05 20:39:35 +0000

    part 5c 5.15

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Blog from "./Blog";
@@ -26,7 +26,7 @@ test("shows title and author, but not url or likes by default", () => {
   expect(likesElement).not.toBeVisible();
 });
 
-test("shows title and author, but not url or likes by default", async () => {
+test("shows title and author, url and likes after click on show button", async () => {
   const blog = {
     title: "new Blog",
     author: "author1235434",
@@ -51,4 +51,30 @@ test("shows title and author, but not url or likes by default", async () => {
 
   expect(urlElement).toBeVisible();
   expect(likesElement).toBeVisible();
+});
+
+test("calls the like handler twice when like button is clicked twice", async () => {
+  const blog = {
+    title: "new Blog",
+    author: "author1235434",
+    url: "url",
+    likes: 15,
+  };
+
+  const handleLike = vi.fn();
+  const user = userEvent.setup();
+
+  render(<Blog blog={blog} handleLike={handleLike} />);
+
+  const showButton = screen.getByText("show");
+  await user.click(showButton);
+
+  const likeButton = screen.getByText("like");
+
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  console.log(handleLike.mock.calls);
+
+  expect(handleLike.mock.calls).toHaveLength(2);
 });
