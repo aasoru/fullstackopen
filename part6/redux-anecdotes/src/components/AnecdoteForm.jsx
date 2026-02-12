410a939bb540a2afb80c00c7ec6a16b783cf0477
Aarón <aaron@MacBook-Pro.local>
2026-02-12 21:29:35 +0000

    part6 6.3-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const addAnecdote = (e) => {
+    e.preventDefault();
+    const content = e.target.content.value;
+    e.target.content.value = "";
+
+    dispatch(createAnecdote(content));
+  };
+
+  return (
+    <>
+      <h2>create new</h2>
+
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="content" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;
